577b0b082c898654ba8436a17aac72bcdc590c67
Guillerius5 <guillerhamel@gmail.com>
2025-12-11 12:49:56 +0100

    Test vacios pero empezaos

---
+++ src/test/java/es/fpsumma/dam2/videoclub/persistence/jpa/repository/AlquilerRepositoryTest.java
@@ -0,0 +1,7 @@
+package es.fpsumma.dam2.videoclub.persistence.jpa.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class AlquilerRepositoryTest {
+}
